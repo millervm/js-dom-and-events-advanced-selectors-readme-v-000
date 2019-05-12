6e0f8b621a7a5d539722eb359b570c5133e05d87
Veronica Miller <vmcmiller@gmail.com>
2019-05-12 21:49:09 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -11,3 +11,19 @@ $('div:last');
 function paragraphSelector() {
   return $('p');
 };
+
+function lastImageSelector() {
+  return $('img:last');
+};
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+};
+
+function divSelector() {
+  return $('div .pics');
+};
+
+function firstListItem() {
+  return $('#pic-list:first-child');
+};
